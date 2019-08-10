190a0f280be5437426688c7190d62e04f965a0d7
ismail <ismaiiil@live.com>
2019-08-10 21:03:20 +0400

    - cleaned upload.xhtml to made test example clearer - ImageManager class to save files - enum for file verification - added maven dependencies for file and string manipulation

--- src/main/java/com/supinfo/suppictures/Controllers/UploadController.java
+++ src/main/java/com/supinfo/suppictures/Controllers/UploadController.java
@@ -1,14 +1,18 @@
 package com.supinfo.suppictures.Controllers;
 
+import com.supinfo.suppictures.Model.ImageFileApi.ImageManager;
+import com.supinfo.suppictures.Model.ImageFileApi.VerifyStatus;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
 import javax.servlet.http.Part;
 import java.io.*;
 
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class UploadController {
     private static final long serialVersionUID = 123456789L;
     private String message;
@@ -26,49 +30,25 @@ public class UploadController {
     public void setMessage(String message) {
         this.message = message;
     }
-    private String getFileNameFromPart(Part part) {
-        final String partHeader = part.getHeader("content-disposition");
-        for (String content : partHeader.split(";")) {
-            if (content.trim().startsWith("filename")) {
-                String fileName = content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
-                return fileName;
-            }
-        }
-        return null;
-    }
-    public String uploadFile() throws IOException {
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
-        FacesContext context = FacesContext.getCurrentInstance();
-        ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
-        String path = servletContext.getRealPath("");
-        boolean file1Success = false;
-        if (file1.getSize() > 0) {
-            String fileName = getFileNameFromPart(file1);
-            File outputFile = new File(path + File.separator + "WEB-INF" + File.separator + fileName);
-            inputStream = file1.getInputStream();
-            outputStream = new FileOutputStream(outputFile);
-            byte[] buffer = new byte[1024];
-            int bytesRead = 0;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-            if (outputStream != null) {
-                outputStream.close();
-            }
-            if (inputStream != null) {
-                inputStream.close();
-            }
-            file1Success = true;
-        }
-        if (file1Success) {
-            System.out.println("File uploaded to : " + path);
-            setMessage("File successfully uploaded to " + path);
-        } else {
 
-            setMessage("Error, select atleast one file!");
+    public void uploadFile(){
+        VerifyStatus verifiedStatus = ImageManager.verify(file1);
+        if(verifiedStatus == VerifyStatus.VALID){
+            try{
+                String path = ImageManager.savePartAs(file1,"defaulttitle");
+                setMessage("/supuploads/" + path);
+            }catch (IOException e){
+                setMessage("an exception has occured");
+            }
+
+        }else{
+            if(verifiedStatus == VerifyStatus.ERROR_INVALID_EXTENSION){
+                setMessage("file has invalid extension");
+            }
+            if(verifiedStatus == VerifyStatus.ERROR_TOO_BIG){
+                setMessage("file too big");
+            }
         }
-        return null;
     }
 
 
