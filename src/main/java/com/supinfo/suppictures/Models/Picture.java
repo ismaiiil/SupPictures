6c36f0df94b710585d99d9f4a8716cfa01520678
Kaushik <kaushik.hurnaum@gmail.com>
2019-08-11 08:38:38 +0400

    Feature/user dao methods (#24)
    * Refactored packages
    
    * Fixed Data Type for PhoneNumber and Category
    
    * Deleting users
    
    * Updating users added
    
    * Refactored models to include relationships

--- src/main/java/com/supinfo/suppictures/Models/Picture.java
+++ src/main/java/com/supinfo/suppictures/Models/Picture.java
@@ -22,6 +22,29 @@ public class Picture {
 
     private Category category;
 
+    private Integer visitorsCount;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "username")
+    private User user;
+
+
+    public Integer getVisitorsCount() {
+        return visitorsCount;
+    }
+
+    public void setVisitorsCount(Integer visitorsCount) {
+        this.visitorsCount = visitorsCount;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public Integer getId() {
         return id;
     }
